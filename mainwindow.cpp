672eade3cb5c599daac8c15deec6a76bc88360f5
Shayan Pakrad <shaayanpakrad@gmail.com>
2024-07-27 15:32:52 +0330

    feat: Add background to main window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPalette>
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // set the fixed size of the screen
+    this->setFixedSize(1000, 600);
+
+    // Set the background image
+    QPixmap bkgnd("E:/Shayan/github_projects/Rakab Graphical/graphical-condottiere/assets/background.jpg");
+    bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
+    QPalette palette;
+    palette.setBrush(QPalette::Window, bkgnd);
+    this->setPalette(palette);
 }
 
 MainWindow::~MainWindow()
