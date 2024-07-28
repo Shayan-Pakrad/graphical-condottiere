f4a2f48b1576ea137b207e4b9e02154d457f202e
Shayan Pakrad <shaayanpakrad@gmail.com>
2024-07-28 12:56:53 +0330

    feat: set size and background of the GameWindow

--- gamewindow.cpp
+++ gamewindow.cpp
@@ -6,6 +6,16 @@ GameWindow::GameWindow(QWidget *parent)
     , ui(new Ui::GameWindow)
 {
     ui->setupUi(this);
+
+    // set the fixed size of the screen
+    this->setFixedSize(1000, 600);
+
+    // Set the background image
+    QPixmap bkgnd(":/assets/background.jpg");
+    bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
+    QPalette palette;
+    palette.setBrush(QPalette::Window, bkgnd);
+    this->setPalette(palette);
 }
 
 GameWindow::~GameWindow()
