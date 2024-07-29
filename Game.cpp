9f1baf3520a49214cda7ba758eecb87b16ca01f3
Shayan Pakrad <shaayanpakrad@gmail.com>
2024-07-30 01:42:03 +0330

    fix: remove welcome function of Game class

--- Game.cpp
+++ Game.cpp
@@ -12,7 +12,6 @@ Game::~Game() {
 
 // Start the game by initializing and sorting players
 void Game::startGame() {
-    welcome();
     sortPlayers();
     showCards();
     while (true) {
@@ -24,22 +23,6 @@ void Game::startGame() {
     }
     endGame();
     
-}
-
-void Game::welcome() {
-        std::string welcomeText = R"(
- __        __         _                                       _____             _____   _                 ____                       _           _     _                             
- \ \      / /   ___  | |   ___    ___    _ __ ___     ___    |_   _|   ___     |_   _| | |__     ___     / ___|   ___    _ __     __| |   ___   | |_  (_)   ___   _ __   _ __    ___ 
-  \ \ /\ / /   / _ \ | |  / __|  / _ \  | '_ ` _ \   / _ \     | |    / _ \      | |   | '_ \   / _ \   | |      / _ \  | '_ \   / _` |  / _ \  | __| | |  / _ \ | '__| | '__|  / _ \
-   \ V  V /   |  __/ | | | (__  | (_) | | | | | | | |  __/     | |   | (_) |     | |   | | | | |  __/   | |___  | (_) | | | | | | (_| | | (_) | | |_  | | |  __/ | |    | |    |  __/
-    \_/\_/     \___| |_|  \___|  \___/  |_| |_| |_|  \___|     |_|    \___/      |_|   |_| |_|  \___|    \____|  \___/  |_| |_|  \__,_|  \___/   \__| |_|  \___| |_|    |_|     \___|
-                                                                                                                                                                                     
-     
-)";
-
-    std::cout << welcomeText << std::endl;
-    std::cout.flush(); // Flush output buffer
-    std::cout << std::endl;
 }
 
 // Initialize players
